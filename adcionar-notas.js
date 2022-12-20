275944b7db88e1c31eb50215c888f03ad7fd2964
Pedro Amorim <86755845+Diposu@users.noreply.github.com>
2022-12-20 20:02:32 -0300

    commit 3

---
+++ adcionar-notas.js
@@ -0,0 +1,13 @@
+const notas = [10, 6 , 8, 7];
+
+notas.push(7); // colocar valor
+
+notas.pop(); // tirar o ultimo valor
+
+console.log(notas)
+
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3] ) / notas.length;
+
+console.log(media)
+
